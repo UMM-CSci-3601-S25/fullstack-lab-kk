b941f1f511fd6f041b8d20ded18b6ec1df3bbd36
K.K. Lamberty <lamberty@morris.umn.edu>
2025-02-14 01:04:57 -0600

    Make simple test pass for UserListSimpleComponent

--- client/src/app/user-list-simple/user-list-simple.component.spec.ts
+++ client/src/app/user-list-simple/user-list-simple.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserListSimpleComponent } from './user-list-simple.component';
+import { RouterModule } from '@angular/router';
+import { MockUserService } from 'src/testing/user.service.mock';
+import { UserService } from '../users/user.service';
 
 describe('UserListSimpleComponent', () => {
   let component: UserListSimpleComponent;
@@ -8,7 +10,8 @@ describe('UserListSimpleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserListSimpleComponent]
+      imports: [RouterModule.forRoot([]), UserListSimpleComponent],
+      providers: [{ provide: UserService, useValue: new MockUserService() }],
     })
       .compileComponents();
 
